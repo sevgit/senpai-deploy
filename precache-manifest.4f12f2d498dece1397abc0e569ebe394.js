6e1e49b6836fbb457a4a599af6d2963fd62a8780
sevgit <sevg1993@gmail.com>
2020-06-22 20:08:12 -0300

    Updates

---
+++ precache-manifest.4f12f2d498dece1397abc0e569ebe394.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1570687d9f2f11c7431ee2f4bf5da121",
+    "url": "/senpai-deploy/index.html"
+  },
+  {
+    "revision": "6d7ef0d07c3daf0dea30",
+    "url": "/senpai-deploy/static/css/main.fe6d25ea.chunk.css"
+  },
+  {
+    "revision": "1371ed15b373bbd4d7ce",
+    "url": "/senpai-deploy/static/js/2.e6495189.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/senpai-deploy/static/js/2.e6495189.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d7ef0d07c3daf0dea30",
+    "url": "/senpai-deploy/static/js/main.63f5ead5.chunk.js"
+  },
+  {
+    "revision": "c55b5641858e0e9112df",
+    "url": "/senpai-deploy/static/js/runtime-main.d607bfec.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/senpai-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
